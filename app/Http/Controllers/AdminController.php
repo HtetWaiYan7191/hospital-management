edcac9d15f53f2fa17b57bcaac17334ba3b97036
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-15 16:53:16 +0630

    Add upload method in Admin controller to add data in database

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Doctor;
+
+
+class AdminController extends Controller
+{
+    //
+    public function addView() {
+        return view('admin.add_doctor');
+    }
+
+    public function upload(Request $request) {
+        $doctor = new Doctor;
+        $image = $request->file;
+        $imageName = time().'.'.$image->getClientOriginalExtension();
+        $request->file->move('doctorImage', $imageName); // path, filename
+        $doctor->image = $imageName;
+        $doctor->name = $request->name;
+        $doctor->phone = $request->phone;
+        $doctor->speciality = $request->speciality;
+        $doctor->room = $request->room;
+        $doctor->save();
+
+        return redirect()->back();
+
+    }
+}
