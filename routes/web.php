5162eb26483f1dc904dea3465a5fd04d09f5d822
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-15 14:40:05 +0630

    Add required routes

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,8 @@ use App\HTTP\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+Route::get('/', [HomeController::class, 'index']);
 Route::get('/home', [HomeController::class, 'redirect']);
 
 Route::middleware([
