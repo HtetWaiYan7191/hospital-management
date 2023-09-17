0819df7e8ef1e8949bb78435b0220bfd0a22e270
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-09-17 20:40:39 +0630

    Add Appointment Controller for functionality related to appointment

---
+++ app/Http/Controllers/AppointmentController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; // Import the Auth facade
+use App\Models\Appointment;
+
+class AppointmentController extends Controller
+{
+    //
+    public function appointmentAdd(Request $request) {
+        $appointment = new Appointment;
+        $appointment->name = $request->name;
+        $appointment->email = $request->email;
+        $appointment->phone = $request->phone;
+        $appointment->date = $request->date;
+        $appointment->doctor = $request->doctor;
+        $appointment->status = false;
+        $appointment->message = $request->message;
+        $appointment->user_id = Auth::user()->id;
+        $appointment->save();
+        return redirect()->back();
+    }
+}
